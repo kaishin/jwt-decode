d69be2ecc1df6dd4cab040cf96ccf8f0d36f6792
Reda Lemeden <git@redalemeden.com>
2020-01-28 15:41:14 +0100

    Remove dependencies and test target

--- Package.swift
+++ Package.swift
@@ -4,27 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "JWTDecode",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "JWTDecode",
-            targets: ["JWTDecode"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Quick/Quick", from: "2.1.0"),
-         .package(url: "https://github.com/Quick/Nimble", from: "8.0.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "JWTDecode",
-            dependencies: [],
-            path: "JWTDecode"),
-        .testTarget(
-            name: "JWTDecode.swiftTests",
-            dependencies: ["JWTDecode", "Quick", "Nimble"]),
-    ]
+  name: "jwt-decode",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "JWTDecode",
+      targets: ["JWTDecode"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "JWTDecode",
+      dependencies: [],
+      path: "JWTDecode"),
+  ]
 )
