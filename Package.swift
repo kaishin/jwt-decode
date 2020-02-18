22e0540da78224c2e9d2aa86dc4f367a17cdaf45
Reda Lemeden <git@redalemeden.com>
2020-02-18 19:44:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "jwt-decode",
+  name: "JWTDecode",
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
